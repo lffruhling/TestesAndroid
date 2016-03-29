c7634c7e65ec4cca8f17826fba0cd2b0ae8f8eea
lffruhling <lffruhling@hotmail.com>
2016-03-29 01:16:14 -0300

    primeiro teste com o text_view

---
+++ teste_retrofit_3/app/src/main/java/com/webdroidteam/teste_retrofit_3/atividade2.java
@@ -0,0 +1,21 @@
+package com.webdroidteam.teste_retrofit_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Leonardo on 28/03/2016.
+ */
+public class atividade2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_atividade2);
+    }
+
+    public void  voltar (View view){
+        finish();
+    }
+
+}
