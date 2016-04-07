a0dc0f55e9133f484124bd68773a75eadba82d09
lffruhling <lffruhling@hotmail.com>
2016-04-07 10:58:52 -0300

    consulta de login - parcial

--- teste_layout_1/app/build/generated/source/r/debug/com/webdroidteam/teste_layout_1/R.java
+++ teste_layout_1/app/build/generated/source/r/debug/com/webdroidteam/teste_layout_1/R.java
@@ -1615,7 +1615,7 @@ containing a value of this type.
         public static final int notification_template_icon_bg=0x7f02004c;
     }
     public static final class id {
-        public static final int action0=0x7f0c0056;
+        public static final int action0=0x7f0c0057;
         public static final int action_bar=0x7f0c0041;
         public static final int action_bar_activity_content=0x7f0c0000;
         public static final int action_bar_container=0x7f0c0040;
@@ -1624,7 +1624,7 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f0c0022;
         public static final int action_bar_title=0x7f0c0021;
         public static final int action_context_bar=0x7f0c0042;
-        public static final int action_divider=0x7f0c005a;
+        public static final int action_divider=0x7f0c005b;
         public static final int action_menu_divider=0x7f0c0002;
         public static final int action_menu_presenter=0x7f0c0003;
         public static final int action_mode_bar=0x7f0c003e;
@@ -1634,11 +1634,12 @@ containing a value of this type.
         public static final int alertTitle=0x7f0c0030;
         public static final int always=0x7f0c001c;
         public static final int beginning=0x7f0c0019;
-        public static final int btnLogin=0x7f0c0053;
+        public static final int btnLogin=0x7f0c0054;
         public static final int buttonPanel=0x7f0c002b;
-        public static final int cancel_action=0x7f0c0057;
+        public static final int cancel_action=0x7f0c0058;
         public static final int checkbox=0x7f0c0039;
-        public static final int chronometer=0x7f0c005d;
+        public static final int chronometer=0x7f0c005e;
+        public static final int ckb_ManterConectado=0x7f0c0053;
         public static final int collapseActionView=0x7f0c001d;
         public static final int contentPanel=0x7f0c0031;
         public static final int cpoSenha=0x7f0c0052;
@@ -1650,7 +1651,7 @@ containing a value of this type.
         public static final int disableHome=0x7f0c000c;
         public static final int edit_query=0x7f0c0043;
         public static final int end=0x7f0c001a;
-        public static final int end_padder=0x7f0c0062;
+        public static final int end_padder=0x7f0c0063;
         public static final int expand_activities_button=0x7f0c0025;
         public static final int expanded_menu=0x7f0c0038;
         public static final int home=0x7f0c0004;
@@ -1658,14 +1659,14 @@ containing a value of this type.
         public static final int icon=0x7f0c0029;
         public static final int ifRoom=0x7f0c001e;
         public static final int image=0x7f0c0026;
-        public static final int imageView=0x7f0c0054;
+        public static final int imageView=0x7f0c0055;
         public static final int imageView2=0x7f0c0050;
-        public static final int info=0x7f0c0061;
-        public static final int line1=0x7f0c005b;
-        public static final int line3=0x7f0c005f;
+        public static final int info=0x7f0c0062;
+        public static final int line1=0x7f0c005c;
+        public static final int line3=0x7f0c0060;
         public static final int listMode=0x7f0c0009;
         public static final int list_item=0x7f0c0028;
-        public static final int media_actions=0x7f0c0059;
+        public static final int media_actions=0x7f0c005a;
         public static final int middle=0x7f0c001b;
         public static final int multiply=0x7f0c0014;
         public static final int never=0x7f0c001f;
@@ -1699,14 +1700,14 @@ containing a value of this type.
         public static final int src_atop=0x7f0c0016;
         public static final int src_in=0x7f0c0017;
         public static final int src_over=0x7f0c0018;
-        public static final int status_bar_latest_event_content=0x7f0c0058;
+        public static final int status_bar_latest_event_content=0x7f0c0059;
         public static final int submit_area=0x7f0c004c;
         public static final int tabMode=0x7f0c000b;
-        public static final int text=0x7f0c0060;
-        public static final int text2=0x7f0c005e;
+        public static final int text=0x7f0c0061;
+        public static final int text2=0x7f0c005f;
         public static final int textSpacerNoButtons=0x7f0c0034;
-        public static final int textView=0x7f0c0055;
-        public static final int time=0x7f0c005c;
+        public static final int textView=0x7f0c0056;
+        public static final int time=0x7f0c005d;
         public static final int title=0x7f0c002a;
         public static final int title_template=0x7f0c002f;
         public static final int topPanel=0x7f0c002e;
@@ -1788,7 +1789,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f060011;
         public static final int abc_toolbar_collapse_description=0x7f060012;
         public static final int app_name=0x7f060014;
+        public static final int confSenha=0x7f060015;
+        public static final int cpoVazio=0x7f060016;
+        public static final int errorLogin=0x7f060017;
+        public static final int save_error=0x7f060018;
+        public static final int save_success=0x7f060019;
+        public static final int senhaVazio=0x7f06001a;
         public static final int status_bar_notification_info_overflow=0x7f060013;
+        public static final int update_success=0x7f06001b;
+        public static final int usuarioVazio=0x7f06001c;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f090086;
--- teste_layout_1/app/src/main/java/com/webdroidteam/teste_layout_1/LoginActivity.java
+++ teste_layout_1/app/src/main/java/com/webdroidteam/teste_layout_1/LoginActivity.java
@@ -2,29 +2,94 @@ package com.webdroidteam.teste_layout_1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.webdroidteam.teste_layout_1.dao.UsuarioDAO;
+import com.webdroidteam.teste_layout_1.util.Mensagem;
 
 /**
  * Created by Leonardo on 30/03/2016.
  */
 public class LoginActivity extends Activity {
+    private EditText cpoUsuario, cpoSenha;
     private UsuarioDAO helper;
     private CheckBox ckbConectado;
+
+    private static final String MANTER_CONECTADO = "manter_conectado";
+    private static final String PREFERENCE_NAME = "LoginActivtyPreferences";
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cpoUsuario    = (EditText) findViewById(R.id.cpoUsuario);
+        cpoSenha      = (EditText) findViewById(R.id.cpoSenha);
+        ckbConectado  = (CheckBox) findViewById(R.id.ckb_ManterConectado);
+
+        helper = new UsuarioDAO(this);
+
+        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+        boolean conectado = preferences.getBoolean(MANTER_CONECTADO, false);
+
+        if (conectado){
+            abreMenu();
+        }
     }
 
-    public void verificaLogin (View view){
+    public void logar(View view){
+        String usuario = cpoUsuario.getText().toString();
+        String senha = cpoSenha.getText().toString();
+
+        boolean validacao = true;
+
+        if (usuario == null || usuario.equals("")){
+            validacao = false;
+            cpoUsuario.setError(getString(R.string.usuarioVazio));
+        }
+
+        if (senha == null || senha.equals("")){
+            validacao = false;
+            cpoSenha.setError(getString(R.string.senhaVazio));
+        }
+
+        if (validacao){
+            // Logar
+            if(helper.logar(usuario,usuario,senha)){
+                //Criar arquivo de preferencia, caso check marcado
+                if (ckbConectado.isChecked()){
+                    SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.putBoolean(MANTER_CONECTADO, true);
+                    editor.commit();
+                }
+
+                abreMenu();
+
+            }else{
+                //Mensagem Erro
+                Mensagem.alertLongo(this, getString(R.string.errorLogin));
+            }
+        }
+
+    }
+
+    private void abreMenu(){
+        Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_menu);
+        finish();
+    }
+
+    /*public void verificaLogin (View view){
         Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
         startActivity(intent);
         setContentView(R.layout.activity_menu);
 
-    }
+    }*/
 }
